8f8dfbcf08817004f3132f2bc8b359103d1a3ad4
kyruski <jamescboyett@gmail.com>
2021-05-10 21:00:32 -0700

    Adds a requirements.txt and adds CORS to flask

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='../client/dist/assets',
             template_folder='../client/dist')
+
+CORS(app)
 
 
 @app.route('/')
